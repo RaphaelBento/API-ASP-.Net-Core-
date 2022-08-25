36eff3945a30251d6bbf934a1d4c0d4a6f5a675f
RaphaelBento <rafhaesilvalbento@gmail.com>
2022-08-25 17:57:27 -0300

    api em aspnet core  será implementado entity framewok core

---
+++ Api_DotNetCore/Controllers/ClientesController.cs
@@ -0,0 +1,26 @@
+﻿using Api_DotNetCore.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_DotNetCore.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        [HttpGet]
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+
+    }
+}
---
+++ Api_DotNetCore/Model/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_DotNetCore.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        public Cliente(int id, string nome , string sobreNome)
+        {
+            Id = id;
+            Nome = nome;
+            SobreNome = sobreNome;
+        }
+    }
+}
